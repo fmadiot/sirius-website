0108cced42fd504cba15dd27b4e80bab609c7375
Pierre-Charles David <pierre-charles.david@obeo.fr>
2013-10-25 17:06:36 +0200

    New version

---
+++ js/magnific-popup.video.js
@@ -0,0 +1,13 @@
+jQuery(document)
+		.ready(function($) {
+	$('.popup-youtube').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
+});
+
