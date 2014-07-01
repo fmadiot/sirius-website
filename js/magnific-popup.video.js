fdf7f9afeea2b878ea77c9dd35694f7a8fadde7e
Pierre-Charles David <pierre-charles.david@obeo.fr>
2014-07-01 14:41:29 +0200

    Add features page

--- js/magnific-popup.video.js
+++ js/magnific-popup.video.js
@@ -23,9 +23,51 @@ jQuery(document)
 		image: {
 			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
 			titleSrc: function(item) {
-				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+				return item.el.attr('title') + ' ';
 			}
 		}
 	});
+	
+	$('.image-popup-vertical-fit').magnificPopup( {
+			type : 'image',
+			closeOnContentClick : true,
+			mainClass : 'mfp-img-mobile',
+			image : {
+				verticalFit : true
+			}
+
+		});
+
+	$('.image-popup-fit-width').magnificPopup( {
+		type : 'image',
+		closeOnContentClick : true,
+		image : {
+			verticalFit : false
+		}
+	});
+
+	$('.image-popup-no-margins').magnificPopup( {
+		type : 'image',
+		closeOnContentClick : true,
+		closeBtnInside : false,
+		fixedContentPos : true,
+		mainClass : 'mfp-no-margins mfp-with-zoom', // class to
+													// remove
+													// default
+													// margin
+													// from left
+													// and right
+													// side
+		image : {
+			verticalFit : true
+		},
+		zoom : {
+			enabled : true,
+			duration : 300
+		// don't foget to change the duration also in CSS
+		}
+	});
+
+	
 });
 
