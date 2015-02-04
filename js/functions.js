64ea58ff577e5c70a39f08bee0b127acbd916779
Pierre-Charles David <pierre-charles.david@obeo.fr>
2015-02-04 15:10:54 +0100

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -3,21 +3,10 @@ $(document).ready(function() {
 	// When user clicks on one of the tags below the associated code is executed
 
 		// Change the download tabs
-		$("#drag2install").click(function() {
-			$("#install_emc").removeClass('active');
-			$("#updatesite").removeClass('active');
-			$(this).addClass("active");
-
-			$("#content_drag2install").fadeIn(700);
-			$("#content_emc").hide();
-			$("#content_updatesite").hide();
-
-			return false;
-		});
-
 		$("#eclipse_drag2install").click(function() {
 			$("#eclipse_install_emc").removeClass('active');
 			$("#eclipse_updatesite").removeClass('active');
+			$("#eclipse_package").removeClass('active');
 			$(this).addClass("active");
 
 			$("#content_drag2install_1").fadeIn(700);
@@ -28,22 +17,10 @@ $(document).ready(function() {
 			return false;
 		});
 
-		$("#install_emc").click(function() {
-			$("#drag2install").removeClass('active');
-			$("#updatesite").removeClass('active');
-			$(this).addClass("active");
-
-			$("#content_emc").fadeIn(700);
-			$("#content_drag2install").hide();
-			$("#content_updatesite").hide();
-			
-			return false;
-
-		});
-
 		$("#eclipse_install_emc").click(function() {
 			$("#eclipse_drag2install").removeClass('active');
 			$("#eclipse_updatesite").removeClass('active');
+			$("#eclipse_package").removeClass('active');
 			$(this).addClass("active");
 
 			$("#content_emc_1").fadeIn(700);
@@ -53,20 +30,10 @@ $(document).ready(function() {
 
 		});
 
-		$("#updatesite").click(function() {
-			$("#drag2install").removeClass('active');
-			$("#install_emc").removeClass('active');
-			$(this).addClass("active");
-
-			$("#content_updatesite").fadeIn(700);
-			$("#content_drag2install").hide();
-			$("#content_emc").hide();
-
-		});
-
 		$("#eclipse_updatesite").click(function() {
 			$("#eclipse_drag2install").removeClass('active');
 			$("#eclipse_install_emc").removeClass('active');
+			$("#eclipse_package").removeClass('active');
 			$(this).addClass("active");
 
 			$("#content_updatesite_1").fadeIn(700);
